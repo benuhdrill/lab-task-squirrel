da97e88c5f1d52b0eb31362f757bd87fd6f175e5
benuhdrill <bgmach2023@fau.edu>
2024-09-03 13:17:15 -0400

    Initial Commit

---
+++ lab-task-squirrel/PhotoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoViewController.swift
+//  lab-task-squirrel
+//
+//  Created by Ben Gmach on 9/3/24.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+    
+    @IBOutlet weak var photoView: UIImageView!
+    var task: Task!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        photoView.image = task.image
+    }
+}
